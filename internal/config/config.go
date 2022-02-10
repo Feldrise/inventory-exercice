c929fd9c39f1c2c741aa17c332ea2bd71a9e4e82
DENIS Victor <victordenis01@gmail.com>
2022-02-10 11:54:26 +0100

    ✨ Add configurations

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+var Cfg Config
+
+type Config struct {
+	Database struct {
+		Name             string `yaml:"name"`
+		ConnectionString string `yaml:"connectionString"`
+	} `yaml:"database"`
+	Collections struct {
+		Inventories    string `yaml:"inventories"`
+		InventoryItems string `yaml:"inventoryItems"`
+		Users          string `yaml:"users"`
+	} `yaml:"collections"`
+}
+
+func Init(configPath string) {
+	file, err := os.Open(configPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&Cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
